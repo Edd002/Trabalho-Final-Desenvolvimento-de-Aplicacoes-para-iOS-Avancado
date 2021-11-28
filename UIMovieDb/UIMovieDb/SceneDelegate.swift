303628fe63f0864c359bc432420cc4e8d6206131
Edd002 <42621512+Edd002@users.noreply.github.com>
2021-11-28 13:12:43 -0300

    Commit 1 - 28/11/2021

--- UIMovieDb/UIMovieDb/SceneDelegate.swift
+++ UIMovieDb/UIMovieDb/SceneDelegate.swift
@@ -12,7 +12,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -27,6 +26,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = UIHostingController(rootView: contentView)
+            window.overrideUserInterfaceStyle = .dark
             self.window = window
             window.makeKeyAndVisible()
         }
--- UIMovieDb/UIMovieDb/Views/MovieListView.swift
+++ UIMovieDb/UIMovieDb/Views/MovieListView.swift
@@ -18,26 +18,15 @@ struct MovieListView: View {
         NavigationView {
             List {
                 Group {
-                    if nowPlayingState.movies != nil {
-                        MoviePosterCarouselView(title: "Em Cartaz", movies: nowPlayingState.movies!)
+                    if popularState.movies != nil {
+                        MovieBackdropCarouselView(title: "Populares", movies: popularState.movies!)
                     } else {
-                        LoadingView(isLoading: self.nowPlayingState.isLoading, error: self.nowPlayingState.error) {
-                            self.nowPlayingState.loadMovies(with: .nowPlaying)
+                        LoadingView(isLoading: self.popularState.isLoading, error: self.popularState.error) {
+                            self.popularState.loadMovies(with: .popular)
                         }
                     }
                 }
                 .listRowInsets(EdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0))
-
-                Group {
-                    if upcomingState.movies != nil {
-                        MovieBackdropCarouselView(title: "Em Breve", movies: upcomingState.movies!)
-                    } else {
-                        LoadingView(isLoading: self.upcomingState.isLoading, error: self.upcomingState.error) {
-                            self.upcomingState.loadMovies(with: .upcoming)
-                        }
-                    }
-                }
-                .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
 
                 Group {
                     if topRatedState.movies != nil {
@@ -51,17 +40,28 @@ struct MovieListView: View {
                 .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
 
                 Group {
-                    if popularState.movies != nil {
-                        MovieBackdropCarouselView(title: "Populares", movies: popularState.movies!)
+                    if nowPlayingState.movies != nil {
+                        MoviePosterCarouselView(title: "Em Cartaz", movies: nowPlayingState.movies!)
                     } else {
-                        LoadingView(isLoading: self.popularState.isLoading, error: self.popularState.error) {
-                            self.popularState.loadMovies(with: .popular)
+                        LoadingView(isLoading: self.nowPlayingState.isLoading, error: self.nowPlayingState.error) {
+                            self.nowPlayingState.loadMovies(with: .nowPlaying)
+                        }
+                    }
+                }
+                .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+
+                Group {
+                    if upcomingState.movies != nil {
+                        MovieBackdropCarouselView(title: "Em Breve", movies: upcomingState.movies!)
+                    } else {
+                        LoadingView(isLoading: self.upcomingState.isLoading, error: self.upcomingState.error) {
+                            self.upcomingState.loadMovies(with: .upcoming)
                         }
                     }
                 }
                 .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 16, trailing: 0))
             }
-            .navigationBarTitle("The MovieDb")
+            .navigationBarTitle("O Seletor de Filmes")
         }
         .onAppear {
             self.nowPlayingState.loadMovies(with: .nowPlaying)
